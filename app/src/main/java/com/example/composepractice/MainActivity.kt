c253fe1cbd2848de55ef1932cf7040cb4011b109
Sebastian Salgado <salgadoseba89@gmail.com>
2024-12-15 22:25:37 -0300

    initial commit

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Buenas Noche $name!",
+        text = "Buenas Noches $name!",
         modifier = modifier
     )
 }
